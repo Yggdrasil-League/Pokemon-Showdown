3405b7ac71daf5e96d1bd9655db5b6ecab9faf88
Siiilver <brahadeeshk@gmail.com>
2015-04-17 00:05:22 +0530

    Update leaguecommands.js

--- custom/leaguecommands.js
+++ custom/leaguecommands.js
@@ -20,16 +20,12 @@ exports.commands = {
 		return;
 	},
 
-/***********************
-	View Gym Leader
-***********************/
-
 	vgl: 'viewgymleaders',
 	viewgymleaders: function (target, room, user) {
 		if (!this.canBroadcast()) return;
 		var glList = '<center><table><tr><td><b>Type</b></td><td><b>Gym Leader<b></td><td><b>Last Seen</b></td></tr><br/>';
 		for (type in gymleaderlist) {
-			var lastSeen = dates.lastSeen(toId(gymleadrlist[type])) + ' ago';
+			var lastSeen = dates.lastSeen(toId(gymleadrlist[type])).split(",")[0] + ' ago';
 			if (Users.get(gymleaderlist[type]) && Users.get(toId(gymleaderlist[type])).connected) 
 				lastSeen = '<font color = "green"> online.</font>';
 			glList += '<tr><td>' + '<img src="http://yggdrasilleague.no-ip.org:25565/storage-files/Types/' + type + '.png">' + '</td>' + '<td>' + gymleaderlist[type] + ': ' + '</td>' + '<td>' + lastSeen + '</td>' + '</tr>';
