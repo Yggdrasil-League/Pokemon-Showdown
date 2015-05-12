55e24b5bcbf9e2a38d6a86bd3d4150ef7ca24011
Robophill <robophill@live.co.uk>
2015-05-12 11:27:29 +0100

    Update leaguecommands.js
    Should allow multiple leagues to use this function and create new json files if needed.
    Still needs testing as I did this at work.

--- custom/leaguecommands.js
+++ custom/leaguecommands.js
@@ -1,5 +1,5 @@
 var fs = require('fs');
-var gymleaderlist = JSON.parse(fs.readFileSync('storage-files/gymleaders.json'));
+
 
 exports.commands = {
 
@@ -10,19 +10,35 @@ exports.commands = {
 	sgl: 'setgymleader',
 	setgymleader: function (target, room, user) {
 		if (!this.can('makeroom')) return;
-		if (!target) return this.sendReply('/setgymleader [type], [user] - Adds user to GL list');
+		if (!target) return this.sendReply('/setgymleader [league], [type], [user] - Adds user to GL list');
 		var targetsArray = target.split(',');
 		var target0 = targetsArray[0];
 		var target1 = targetsArray[1];
-		this.sendReply(target1 + ' has now been set as ' + target0 + ' Gym Leader.');
-		gymleaderlist[target0] = target1;
-		fs.writeFile('storage-files/gymleaders.json', JSON.stringify(gymleaderlist));
+		var target2 = targetsArray[2];
+		if (path.existsSync(target0 + 'gymleaders.json')) { 
+			var gymleaderlist = JSON.parse(fs.readFileSync('storage-files/'+target0+'gymleaders.json'));
+			gymleaderlist[target0] = target2;
+  			fs.writeFile('storage-files/'+target0+'gymleaders.json', JSON.stringify(gymleaderlist));
+		}else{
+			var gymleaderlist = JSON.parse(fs.readFileSync('storage-files/'+target0+'gymleaders.json'));
+			gymleaderlist[target0] = target2;
+			fs.writeFile('storage-files/'+target0+'gymleaders.json','{'+JSON.stringify(gymleaderlist)+'}');
+		}
+		this.sendReply(target2 + ' has now been set as ' + target1 + ' Gym Leader for ' + target0 + '.');
+		
 		return;
 	},
 
 	vgl: 'viewgymleaders',
 	viewgymleaders: function (target, room, user) {
 		if (!this.canBroadcast()) return;
+		if(!target) return this.sendReply ('/viewgymleaders [league] - Displays the list of Gym Leaders for selected league.');
+		if (path.existsSync(target + 'gymleaders.json')) { 
+  			var gymleaderlist = JSON.parse(fs.readFileSync('storage-files/'+target+'gymleaders.json'));
+		}else{
+			return this.sendReply ('League doesn't exist or hasn't set up a Gym Leader list.');
+		}
+		
 		var glList = '<center><table><tr><td><b>Type</b></td><td><b>Gym Leader<b></td><td><b>Last Seen</b></td></tr><br/>';
 		for (type in gymleaderlist) {
 			var lastSeen = dates.lastSeen(toId(gymleadrlist[type])).split(",")[0] + ' ago';
