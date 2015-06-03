270448a56f03831cd87c0edb7cd2e1f7f799872b
Robophill <robophill@live.co.uk>
2015-06-03 17:30:50 +0100

    Changes so it's not case sensitive.

--- custom/leaguecommands.js
+++ custom/leaguecommands.js
@@ -15,6 +15,7 @@ exports.commands = {
 		var target0 = targetsArray[0];
 		var target1 = targetsArray[1];
 		var target2 = targetsArray[2];
+		target0 = toId(target0);
 		target1 = toId(target1);
 		target1 = target1.charAt(0).toUpperCase() + target1.slice(1).toLowerCase();
 		if(target1 != 'Fairy' && target1 != 'Bug' && target1 != 'Dragon' && target1 != 'Psychic' && target1 != 'Water' && target1 != 'Fire' && target1 != 'Grass' && target1 != 'Ground' && target1 != 'Rock' && target1 != 'Dark' && target1 != 'Ice' && target1 != 'Electric' && target1 != 'Flying' && target1 != 'Normal' && target1 != 'Poison' && target1 != 'Ghost' && target1 != 'Steel' && target1 != 'Fighting') return this.sendReply ('Please select a valid type using capitalisation');
@@ -38,6 +39,7 @@ exports.commands = {
 	viewgymleaders: function (target, room, user) {
 		if (!this.canBroadcast()) return;
 		if(!target) return this.sendReply ('/viewgymleaders [league] - Displays the list of Gym Leaders for selected league.');
+		target = toId(target);
 		if (fs.existsSync('storage-files/'+target+'gymleaders.json')) { 
   			var gymleaderlist = JSON.parse(fs.readFileSync('storage-files/'+target+'gymleaders.json'));
 		}else{
